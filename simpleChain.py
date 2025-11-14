b5866a9e339677e4876cbb6042bb7c284cf1f467
Mobcoder ID-100 <mobcoderid-100@Mobcoders-Mac-mini.local>
2025-11-14 17:47:26 +0530

    simpleChain

---
+++ simpleChain.py
@@ -0,0 +1,22 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser # parses output strings into more structured formats
+
+load_dotenv()
+
+prompt = ChatPromptTemplate.from_template("Generate a report on {topic}")
+model= ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0.7,
+    max_output_tokens=500
+)
+
+
+parser = StrOutputParser()
+chain = prompt | model | parser
+
+result = chain.invoke({"topic": "cats"})
+print(result)
+
+chain.get_graph().print_ascii()
